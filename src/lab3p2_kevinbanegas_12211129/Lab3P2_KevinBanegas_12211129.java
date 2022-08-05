463bbda5b2b000036386448de13acd38ce5a4e43
KevinBanegas <kevinandre0504@gmail.com>
2022-08-05 13:18:03 -0600

    Comienzo

---
+++ src/lab3p2_kevinbanegas_12211129/Lab3P2_KevinBanegas_12211129.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3p2_kevinbanegas_12211129;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lab3P2_KevinBanegas_12211129 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
